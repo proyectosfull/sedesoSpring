b3955713e0670902842c9853a5bd0508be05683e
abibarte <abibarte@gmail.com>
2020-06-03 19:55:26 -0500

    Import sin usar

--- Sedeso/SedesoWEB/src/main/java/com/sedeso/dig/zdao/ReportesDao.java
+++ Sedeso/SedesoWEB/src/main/java/com/sedeso/dig/zdao/ReportesDao.java
@@ -6,8 +6,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.*; 
-import java.util.stream.*; 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
